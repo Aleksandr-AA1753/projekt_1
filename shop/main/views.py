de3908fcb26ee3b5b76b466197c214e5cec9a39f
Aleksandr <aleksandr_almaz@mail.ru>
2024-05-17 21:08:31 +0500

    create my tag goods

--- shop/main/views.py
+++ shop/main/views.py
@@ -8,7 +8,6 @@ def index(request):
     context = {
         'title': 'Home - Главная',
         'content': 'Магазин мебели HOME',
-        'categories': categories
     }
     return render(request, 'main/index.html', context)
 
