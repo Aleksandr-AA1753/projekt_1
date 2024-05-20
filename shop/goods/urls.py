0b92d07f63f614ec5024a4b21d985a9c8cac13a3
Aleksandr <aleksandr_almaz@mail.ru>
2024-05-20 19:17:20 +0500

    first paginator

--- shop/goods/urls.py
+++ shop/goods/urls.py
@@ -7,6 +7,7 @@ admin.site.site_header = "Страница управления"
 app_name = 'goods'
 urlpatterns = [
     path('<slug:category_slug>/', views.catalog, name='index'),
+    path('<slug:category_slug>/<int:page>/', views.catalog, name='index'),
     path('product/<slug:product_slug>/', views.product, name='product'),
 
 ]
