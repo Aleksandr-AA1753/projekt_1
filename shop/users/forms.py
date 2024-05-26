e06cdc73a9476312974d72c4266aceaddaa11faf
Aleksandr <aleksandr_almaz@mail.ru>
2024-05-26 21:29:34 +0500

    create messages

--- shop/users/forms.py
+++ shop/users/forms.py
@@ -1,6 +1,6 @@
 from dataclasses import field
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
 
 from users.models import User
 
@@ -47,3 +47,19 @@ class UserRegistrationForm(UserCreationForm):
         )
 
     
+class ProfileForm(UserChangeForm):
+    image = forms.ImageField(required=False)
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+    username = forms.CharField()
+    email = forms.CharField()
+
+    class Meta:
+        model = User
+        fields = (
+            "image",
+            "first_name",
+            "last_name",
+            "username",
+            "email",
+        )
